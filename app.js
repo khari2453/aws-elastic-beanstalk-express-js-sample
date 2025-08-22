37992034ead612e5e0b81a3e3ececf9a62082242
guviharikumar-at-152415993612 <Khari2453@gmail.com>
2025-08-22 20:31:49 +0530

    new changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello All! Welcome to AWS CI/CD Session'));
+app.get('/', (req, res) => res.send('Hello All! CICD_DEMO'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
